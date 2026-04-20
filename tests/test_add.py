8e25a3b0a8112471d44a958acb4cbafff48a27a5
BigHouseBH <[TU-EMAIL@gmail.com]>
2026-04-20 16:51:22 -0300

    Comente mi aprendisaje y entendimiento basico sobre el funcionamiento del "with"

--- tests/test_add.py
+++ tests/test_add.py
@@ -28,3 +28,22 @@ def test_add_suma_positivos():
 # ])
 # def test_add_parametrizado(a, b, expected):
 #     assert add(a, b) == expected
+
+
+
+#Segun la ia: el assert es una especie  es un operador que funciona 
+# semajnte a un return, cuya funcion es dar unos "casos de prueva"
+# (ejemplos de valores) al la funcion y
+# verificar si retorna lo esperado en tal caso
+#Piensalo como decir: asertas si tal valores retornas tal respuesta
+def  testSumarDosNegativos():
+    assert add(-1, -2) == -3#"Si la evaluo en -1 + -2 debe dar -3"
+    
+def testSumarPositivoYNegativo():
+    assert add(5, -3) == 2#"Si la evaluo en 5 + -3 debe dar 2"
+    
+def testSumarConCero():
+    assert add(1, 0) == 1#"Si la evaluo en 1 + 0 debe dar 1"
+    
+def testSumarConDecimales():
+    assert    add(1.1,1.1)==2.2;
